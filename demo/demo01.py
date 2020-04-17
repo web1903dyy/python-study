d3c9cc8ae16c69d5fea7c6bb3db32888d22a52d0
web1903dyy <d15884560890@126.com>
2020-04-17 10:43:48 +0800

    第一次像github提交代码

---
+++ demo/demo01.py
@@ -0,0 +1,15 @@
+# print("hello world!") #字符串
+# print(123) #整数
+# print(2.333) #小数
+# print(True) #布尔值
+# print(()) #元组
+# print([]) #数组
+# print({}) #字典
+
+# a = float(input("请输入内容："))
+# b = float(input("请输入内容："))
+# print("input获取的内容：",a+b)
+
+a='sdkfjdhgkjdfhgkjfhdgkj'
+b='lkdsflksdflskdjflsdj'
+print(int(len(a))+int(len(b)))
